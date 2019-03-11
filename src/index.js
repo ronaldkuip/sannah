748a5b39131d5d6e006fb61e7e5d241817dd3762
Ronald Kuip <ronald.kuip@github.com>
2019-03-11 13:24:37 +0100

    Initialize (1)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => (
+  <div>
+     <h1>Dag Wereld!!</h1>
+  </div>
+)
+ReactDOM.render(<App/>, document.getElementById('root'));
